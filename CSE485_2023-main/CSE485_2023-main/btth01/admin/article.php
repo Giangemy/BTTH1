6e35f9aee0efdde2506b18041da7abf2f6b78a72
Giangemy <104207306+Giangemy@users.noreply.github.com>
2023-10-03 10:12:44 +0700

    Add files via upload

---
+++ CSE485_2023-main/CSE485_2023-main/btth01/admin/article.php
@@ -0,0 +1,16 @@
+<section class="bg-accent section-space-less2">
+    <div class="container">
+        <div class="modal-header">
+            <div class="title-login-form">Đăng nhập Admin</div>
+        </div>
+        <div class="login-form">
+            <form method="post">
+                <label>admin *</label>
+                <input name="username_admin" type="text" placeholder="admin" />
+                <label>Mật khẩu *</label>
+                <input name="password_admin" type="password" placeholder="12345" />
+                <button type="submit" name="login_admin" value="Login">Đăng nhập</button>
+            </form>
+        </div>
+    </div>
+</section>
